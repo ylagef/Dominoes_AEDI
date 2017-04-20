87752b8cb0d6748b6ba011cb6905b40b44b0ec4b
ylagef <Githubsimple.23>
2017-04-20 17:25:45 +0200

    Init

---
+++ src/Board.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Yeray on 20/04/2017.
+ */
+public class Board {
+}
---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Yeray on 20/04/2017.
+ */
+public class Game {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
