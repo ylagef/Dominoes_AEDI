73ce2290f2a9591c5dd19b8cd279944f977c5527
ylagef <Githubsimple.23>
2017-04-20 17:36:51 +0200

    Init

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,30 @@
+import java.util.LinkedList;
+
 /**
  * Created by Yeray on 20/04/2017.
  */
 public class Player {
+    String name;
+    LinkedList<Piece> hand = new LinkedList<>();
+
+    public Player(String name, LinkedList<Piece> hand) {
+        this.name = name;
+        this.hand = hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LinkedList<Piece> getHand() {
+        return hand;
+    }
+
+    public void setHand(LinkedList<Piece> hand) {
+        this.hand = hand;
+    }
 }
