5e746ec9bb5a3fa197cd854776268a913a3e709d
ylagef <Githubsimple.23>
2017-04-20 17:59:40 +0200

    Board

--- src/Board.java
+++ src/Board.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
  * Created by Yeray on 20/04/2017.
  */
 public class Board {
-jfkhgf
+
     LinkedList<Piece> heap = new LinkedList<>();
     LinkedList<Piece> board = new LinkedList<>();
 
